3fee7418f313f6830b2321edfa239ed8ae41d8aa
Aryan Sethi <aryan.rdps@gmail.com>
2022-03-18 11:09:24 +0530

    Formatted code

--- js/App.js
+++ js/App.js
@@ -2,7 +2,7 @@ const arrows = document.querySelectorAll(".arrow");
 
 for (const arrow of arrows) {
     const parentSubmenu = arrow.parentElement;
-    arrow.addEventListener("click" , e => {
+    arrow.addEventListener("click", e => {
         parentSubmenu.classList.toggle("show");
-    });  
+    });
 }
